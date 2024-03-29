12e1eecc48e70e7da9aa5f287fde8e6e99a1002b
cradle0fFilth <3175806342@qq.com>
2024-03-29 05:33:56 +0100

    add head

--- programs/solot/src/lib.rs
+++ programs/solot/src/lib.rs
@@ -1,8 +1,21 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    metadata::{
+        create_metadata_accounts_v3,
+        mpl_token_metadata::{accounts::Metadata as MetadataAccount, types::DataV2},
+        CreateMetadataAccountsV3, Metadata,
+    },
+    token::{burn, mint_to, Burn, Mint, MintTo, Token, TokenAccount},
+};
+use solana_program::{pubkey, pubkey::Pubkey};
 
 declare_id!("GCDAMzKMKeoX4U8HR4Leop868pygJ5nFpYpnCbwsoiGd");
 
-#[program]
+const ADMIN_PUBKEY: Pubkey = pubkey!("");
+const INITIAL_SOLOT_NUM:u8 = 1000;
+
+#[program]:
 pub mod solot {
     use super::*;
 
